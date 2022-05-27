c5aba16c122cf07a347622a54347663890b759b6
v1vvk17 <v12345@mail.ru>
2022-05-27 12:02:48 +0000

    Cnfig added

---
+++ cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/vvk17/P96-api-gateway/config"
+)
+
+func main() {
+
+	log.Println("Config is loading...")
+	c, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalln("Failed at config", err)
+	}
+	log.Println("Config loaded:", c)
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "привет, это заглавная страница")
+	})
+
+	r.Run(":3001")
+}
